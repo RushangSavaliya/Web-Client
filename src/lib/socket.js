2ee97e4906f7a525a5ab8f8536d4e3c2eff35c01
Rushang Savaliya <rushangsavaliya19@gmail.com>
2025-06-21 16:28:00 +0530

    Fix: use env URL for socket connection

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io(import.meta.env.VITE_SERVER_URL, {
     auth: {
         token: localStorage.getItem("token"),
     },
