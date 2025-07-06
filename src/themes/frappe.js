8eed5a6a2aae2501299bc78397f248592b6d6a27
Rushang Savaliya <rushangsavaliya19@gmail.com>
2025-07-06 16:06:25 +0530

    fix: split catppuccin themes into separate files was working earlier, but stopped locally after a few days

---
+++ src/themes/frappe.js
@@ -0,0 +1,4 @@
+// File: src/themes/catppuccin/frappe.js
+
+import { createCatppuccinPlugin } from '@catppuccin/daisyui';
+export default createCatppuccinPlugin('frappe');
---
+++ src/themes/mocha.js
@@ -0,0 +1,4 @@
+// File: src/themes/catppuccin/mocha.js
+
+import { createCatppuccinPlugin } from '@catppuccin/daisyui';
+export default createCatppuccinPlugin('mocha');
